24607d13bb5c71dfea9efdb571a38e2c22ffbe9f
Ananth Bhamidipati <ananthbh@gmail.com>
2022-07-10 20:08:33 +0200

    added necessary UI to GameViewController

--- GameOfLife/View/BoardViewController/GameViewController.swift
+++ GameOfLife/View/BoardViewController/GameViewController.swift
@@ -7,23 +7,43 @@
 
 import UIKit
 
-class GameViewController: UIViewController {
+final class GameViewController: UIViewController {
 
+    // MARK: Outlets
+    
+    @IBOutlet private weak var boardView: BoardView!
+    @IBOutlet private weak var startButton: UIButton!
+    @IBOutlet private weak var resetButton: UIButton!
+    @IBOutlet private weak var infoLabel: UILabel!
+    
+    // MARK: Life cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupInitialUI()
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: Actions
+    
+    @IBAction private func startAction(_ sender: Any) {
     }
-    */
+    
+    @IBAction private func resetAction(_ sender: Any) {
+    }
+    
+}
 
+private extension GameViewController {
+    private func setupInitialUI() {
+        self.navigationItem.title = "Game of Life"
+        
+        infoLabel.text = "Tap on Start button below"
+        
+        startButton.layer.borderWidth = 1.0
+        startButton.layer.borderColor = UIColor.systemBlue.cgColor
+        
+        resetButton.layer.borderWidth = 1.0
+        resetButton.layer.borderColor = UIColor.systemBlue.cgColor
+    }
 }
